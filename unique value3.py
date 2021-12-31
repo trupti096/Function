b221cffb1eb9c76db9782c1a05ec8163999349cd
Truptimayee Singh <84379470+trupti096@users.noreply.github.com>
2022-01-01 00:03:24 +0530

    Add files via upload

---
+++ unique value3.py
@@ -0,0 +1,29 @@
+#print
+string_list = ["Empathy", "Empathy", "Kindness", "Kindness", "Compassion", "Humble", "Humble"]
+def list(string_list):
+    i=0
+    b=[]
+    while i<len(string_list):
+        j=0
+        while j<len(string_list):
+            if string_list[i] not in b:
+                b.append(string_list[i])
+            j=j+1
+        i=i+1
+    print(b)
+list(string_list)
+
+#return
+string_list = ["Empathy", "Empathy", "Kindness", "Kindness", "Compassion", "Humble", "Humble"]
+def list(string_list):
+    i=0
+    b=[]
+    while i<len(string_list):
+        j=0
+        while j<len(string_list):
+            if string_list[i] not in b:
+                b.append(string_list[i])
+            j=j+1
+        i=i+1
+    return b
+print(list(string_list))
