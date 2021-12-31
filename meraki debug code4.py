90bed11009febe0e11c1b0edb8fd6c844d5b8fa9
Truptimayee Singh <84379470+trupti096@users.noreply.github.com>
2021-12-31 23:04:37 +0530

    Add files via upload

---
+++ meraki debug code4.py
@@ -0,0 +1,15 @@
+def distance(kms):
+
+        if kms < 20:
+
+            print("close")
+
+        elif kms < 50:
+
+            print("median")
+
+        else:
+
+            print("far")
+
+distance(20)
