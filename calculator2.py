ca12fc71ee836d1a41034147e519de4603bd6633
Truptimayee Singh <84379470+trupti096@users.noreply.github.com>
2021-12-31 22:37:12 +0530

    Add files via upload

---
+++ calculator2.py
@@ -0,0 +1,35 @@
+#add
+num1=int(input("enter_1st_no="))
+num2=int(input("enter_2nd_no="))
+def calculator(enter_1st_no,enter_2nd_no):
+    calculator_add=enter_1st_no + enter_2nd_no
+    return calculator_add
+add=calculator(num1,num2)
+print(add,"add")
+
+#subtract
+num1=int(input("enter_1st_no="))
+num2=int(input("enter_2nd_no="))
+def calculator(enter_1st_no,enter_2nd_no):
+    calculator_subtract=enter_1st_no - enter_2nd_no
+    return calculator_subtract
+subtract=calculator(num1,num2)
+print(subtract,"subtract")
+
+#multiply
+num1=int(input("enter_1st_no="))
+num2=int(input("enter_2nd_no="))
+def calculator(enter_1st_no,enter_2nd_no):
+    calculator_multiply=enter_1st_no * enter_2nd_no
+    return calculator_multiply
+multiply=calculator(num1,num2)
+print(multiply,"multiply")
+
+#divide
+num1=int(input("enter_1st_no="))
+num2=int(input("enter_2nd_no="))
+def calculator(enter_1st_no,enter_2nd_no):
+    calculator_divide=enter_1st_no / enter_2nd_no
+    return calculator_divide
+divide=calculator(num1,num2)
+print(divide,"divide")
