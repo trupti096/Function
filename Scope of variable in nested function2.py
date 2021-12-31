3998663340406e9deb381f3270ff35129091b3da
Truptimayee Singh <84379470+trupti096@users.noreply.github.com>
2021-12-31 23:51:45 +0530

    Add files via upload

---
+++ Scope of variable in nested function2.py
@@ -0,0 +1,8 @@
+def first_function():
+    s = 'I love India'
+    def second_function():
+        s = "MY NAME IS JACK"
+        print(s)     
+    second_function()
+    print(s)    
+first_function()
