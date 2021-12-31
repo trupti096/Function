bd125591ff3dcbd724f8090f25bedf282ab7a706
Truptimayee Singh <84379470+trupti096@users.noreply.github.com>
2021-12-31 22:51:59 +0530

    Add files via upload

---
+++ length of string.py
@@ -0,0 +1,16 @@
+#print
+#1
+string_name = "Shakrudin"
+print (len(string_name))
+
+#2
+string_name = "Rishabh Verma"
+print (len(string_name))
+
+#3
+string_name = "navgurukul"
+if "n" in string_name:
+    print ("n hai")
+else:
+    print ("n nahi hai")
+
