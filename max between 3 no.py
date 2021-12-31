041a288ecdff43b3b594479673395f344198a427
Truptimayee Singh <84379470+trupti096@users.noreply.github.com>
2021-12-31 22:56:46 +0530

    Add files via upload

---
+++ max between 3 no.py
@@ -0,0 +1,8 @@
+def function(num1,num2,num3):
+    if num1>num2>num3:
+        print(num1,"bada hai")
+    elif num2>num1>num3:
+        print(num2,"bada hai")
+    elif num3>num2>num1:
+        print(num3,"bada hai")
+function(68,32,24)
