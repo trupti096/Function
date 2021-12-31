756bc3a8bfde577073800fb4c9fb6b1e3619dffe
Truptimayee Singh <84379470+trupti096@users.noreply.github.com>
2021-12-31 22:36:30 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,27 @@
+#add
+def calculator(number_x,number_y):
+    calculator_add=number_x + number_y
+    return calculator_add
+sum1=calculator(24,20)
+print(sum1,"add")
+
+#subtract
+def calculator(number_x,number_y):
+    calculator_subtract=number_x - number_y
+    return calculator_subtract
+sum2=calculator(90,23)
+print(sum2,"subtract")
+
+#multiply
+def calculator(number_x,number_y):
+    calculator_multiply=number_x * number_y
+    return calculator_multiply
+sum3=calculator(50,60)
+print(sum3,"multiply")
+
+#divide
+def calculator(number_x,number_y):
+    calculator_divide=number_x / number_y
+    return calculator_divide
+sum4=calculator(80,120)
+print(sum4,"divide")
