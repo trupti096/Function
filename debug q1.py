1d84c58c9f98a39d4aa1e98cf63b880721d4468d
Truptimayee Singh <84379470+trupti096@users.noreply.github.com>
2021-12-31 22:40:33 +0530

    Add files via upload

---
+++ debug q1.py
@@ -0,0 +1,6 @@
+def greet(names,names1,names2,names3):
+    print("Welcome", names)
+    print("Welcome",names1)
+    print("Welcome",names2)
+    print("Welcome",names3)
+greet("Rinki", "Vishal", "Kartik", "Bijender")
