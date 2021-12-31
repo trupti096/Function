199a4500da059def9ac3de14159625b988cbb4e3
Truptimayee Singh <84379470+trupti096@users.noreply.github.com>
2021-12-31 23:20:00 +0530

    Add files via upload

---
+++ multiple function arguments example3.py
@@ -0,0 +1,13 @@
+def say_hello_people(name_x, name_y, name_z, name_a):
+
+    print ("Namaste ", name_x) # hindi mein
+
+    print ("Alah hafiz ", name_y) # urdu mein
+
+    print ("Bonjour ", name_z) # french mein
+
+    print ("Hello ", name_a) # english mein
+
+say_hello_people("Imitiyaz", "Rishabh", "Rahul", "Vidya")
+
+say_hello_people("Steve", "Saswata", "Shakrundin", "Rajeev")
