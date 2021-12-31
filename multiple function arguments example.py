6c2301f98c5d1a6c30913725a70dee5e43ee1a09
Truptimayee Singh <84379470+trupti096@users.noreply.github.com>
2021-12-31 23:18:45 +0530

    Add files via upload

---
+++ multiple function arguments example.py
@@ -0,0 +1,13 @@
+def add_numbers(number1, number2):
+
+    print ("Main do numbers ko add karunga.")
+
+    print (number1 + number2)
+
+add_numbers(120, 50)
+
+num_x = 134
+
+name = "Rinki"
+
+add_numbers(str(num_x),name)
